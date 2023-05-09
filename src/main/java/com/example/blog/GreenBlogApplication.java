fc5d3d9bc1b7db60b4f9f315226be71d461114df
jiun0516 <leejiun0516@gmail.com>
2023-05-09 15:31:28 +0900

    제약 설정

--- src/main/java/com/example/blog/GreenBlogApplication.java
+++ src/main/java/com/example/blog/GreenBlogApplication.java
@@ -1,4 +1,4 @@
-package com.example.greenblog;
+package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/blog/model/User.java
@@ -0,0 +1,34 @@
+package com.example.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment -> pk 적용
+	private int id;
+	@Column(nullable = false, length = 30)
+	private String username;
+	
+	@Column(nullable = false, length = 100)
+	private String password;
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+
+	@ColumnDefault("'user'") // 문자열 타입 이라고 명시는 ''(홑따옴표)
+	private String role; // user, admin, manager
+	
+	@CreationTimestamp // 자동 시간 입력 == now()
+	private Timestamp createdDate;
+}
